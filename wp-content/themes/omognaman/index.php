508c6109d85cb70929b61d307e6e689ea96738b9
Stefan Ledin <stefan.ledin@gmail.com>
2017-06-10 17:30:53 +0200

    stajlat om. thumbnail finnes

--- wp-content/themes/omognaman/index.php
+++ wp-content/themes/omognaman/index.php
@@ -3,33 +3,35 @@
     <div class="site-wrapper">
         <div class="page">
             <header class="site-header">
-                <h1 class="site-title">Omogna män</h1>
+                <img class="site-logo" src="<?php echo asset('img/logga_1x.png');?>" srcset="<?php  echo asset('img/logga_1x.png') . ' 768w, ' . asset('img/logga_2x.png');?> 1400w" sizes="768px" alt="Omogna män">
             </header>
             <main>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article class="episode">
-                    <figure class="episode__image">
-                        <img src="<?php echo asset('img/logga.png');?>" alt="Omogna män">
-                    </figure>
-                    <div class="episode__description">
-                        <div class="episode__description__inner">
-                            <header class="episode__header">
-                                <h2 class="episode__title"><?php the_title();?></h2>
-                                <div class="episode__metadata">
-                                    <span class="episode__duration"><?php echo 'Längd: ' . get_post_meta( $post->ID, 'duration', true ) . ' min.';?></span>
-                                    <span class="episode__published">
-                                        <?php
-                                        $date_recorded = get_post_meta( $post->ID, 'date_recorded', true );
-                                        echo 'Publicerat: ' . date('Y-m-d', strtotime($date_recorded) );
-                                        ?>
-                                    </span>
-                                </div>
-                            </header>
-                            <?php if ( $audio_file = get_post_meta( $post->ID, 'audio_file', true ) ) : ?>
-                                <audio src="<?php echo $audio_file; ?>" controls="controls" preload="none" class="episode__player">
-                                    Moget
-                                </audio>
+                    <div class="episode__inner">
+                        <header class="episode__header">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <figure class="episode__image">
+                                    <?php the_post_thumbnail(); ?>
+                                </figure>
                             <?php endif; ?>
+                            <h2 class="episode__title"><?php the_title();?></h2>
+                            <div class="episode__metadata">
+                                <span class="episode__duration"><?php echo 'Längd: ' . get_post_meta( $post->ID, 'duration', true ) . ' min.';?></span>
+                                <span class="episode__published">
+                                    <?php
+                                    $date_recorded = get_post_meta( $post->ID, 'date_recorded', true );
+                                    echo 'Publicerat: ' . date('Y-m-d', strtotime($date_recorded) );
+                                    ?>
+                                </span>
+                            </div>
+                        </header>
+                        <?php if ( $audio_file = get_post_meta( $post->ID, 'audio_file', true ) ) : ?>
+                            <audio src="<?php echo $audio_file; ?>" controls="controls" preload="none" class="episode__player">
+                                Moget
+                            </audio>
+                        <?php endif; ?>
+                        <div class="episode__description">
                             <?php the_content(); ?>
                         </div>
                     </div>
